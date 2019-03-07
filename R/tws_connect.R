d2f2facfa4b6c2eb97c8c95267f2b49b778c05aa
pverspeelt <phiver4@gmail.com>
2019-03-07 15:05:28 +0100

    Added is_ connected objects and checks
    changed readBin in tws_connect to read 2 lines instead of twice 1 line. In python there is a check if there are more lines than 2, but not in java. So for now leave it at 2.
    
    Created internal function is_tws_connection_open to check if connection is open. Used this in several other functions as a check.
    
    Added tick type object that is used to check the ticks sent by tws to the api.

--- R/tws_connect.R
+++ R/tws_connect.R
@@ -55,7 +55,7 @@ tws_connect <-
     
     
     # startapi needs to be called after connection. 
-    
+    # 
     # v100prefix = "API\0"
     # v100version = "v%d..%d" % (MIN_CLIENT_VER, MAX_CLIENT_VER)
     # #v100version = "v%d..%d" % (MIN_CLIENT_VER, 101)
@@ -64,6 +64,7 @@ tws_connect <-
     # msg2 = str.encode(v100prefix, 'ascii') + msg
     # logger.debug("REQUEST %s", msg2)
     # self.conn.sendMsg(msg2)
+  
     
     start_api <- function (open_connection, clientId)
       # needs to write start_api, version, clientid 
@@ -104,9 +105,11 @@ tws_connect <-
       # can this be used with R?
       # 
       # if (.client_version$MIN_CLIENT_VER < .client_version$MAX_CLIENT_VER) {
-      #   msg <- paste0('v', .client_version$MIN_CLIENT_VER, "..", .client_version$MAX_CLIENT_VER)
+      #   msg <- sprintf("v%s..%s", .client_version$MIN_CLIENT_VER, .client_version$MAX_CLIENT_VER)
       # }
       # for now using client_version 66 as defined in c++ and java when not using v100Plus protocol
+      # 
+      # 
      
       
       CLIENT_VERSION <- "66"
@@ -119,12 +122,12 @@ tws_connect <-
           break
         if (!socketSelect(list(sock_con), FALSE, 0.1))
           next
-        curMsg <- readBin(sock_con, character(), 1)
+        curMsg <- readBin(sock_con, character(), 2L)
         #cat(curMsg,'\n')
         
         if (is.null(SERVER_VERSION)) {
           SERVER_VERSION <- curMsg[1]
-          CONNECTION_TIME <- readBin(sock_con, character(), 1)
+          CONNECTION_TIME <- curMsg[2]
           next
         }
       }
@@ -138,7 +141,7 @@ tws_connect <-
       tws_con$port <- port
       tws_con$server_version <- SERVER_VERSION
       tws_con$connected_at <- CONNECTION_TIME
-      tws_con$connected <- NULL # check python / c++ code if this is used
+      tws_con$connected <- is_connection_open(tws_con$con)
       class(tws_con) <- c("tws_con", "environment")
 
       # tws needs an API call now
@@ -162,8 +165,8 @@ tws_connect <-
         tws_con$clientId <- NULL
         tws_con$nextValidId <- NULL
         tws_con$port <- NULL
-        tws_con$server.version <- NULL
-        tws_con$connected.at <- filename
+        tws_con$server_version <- NULL
+        tws_con$connected_at <- filename
         class(tws_con) <- c("tws_playblack", "tws_con", "environment")
         return(tws_con)
       }
@@ -180,9 +183,29 @@ is_tws_connection <- function(x) {
 #' @rdname tws_connect
 #' @export
 tws_disconnect <- function(tws_con) {
-  if(!is_tws_connection(tws_con))
-    stop("not a 'tws' connection")
+  if (!is_tws_connection(tws_con)) {    
+    stop("not a 'tws' connection", call. = FALSE)
+  }
   
-  tws_con$connected <- FALSE
-  close(tws_con$con)
+  if (is_tws_connection_open(tws_con)) {
+    tws_con$connected <- FALSE
+    tws_con$connected_at <- NULL
+    close(tws_con$con)
+  } else {
+    glue("The connection to tws is already closed.")
+  }
+}
+
+#' @rdname tws_connect
+#' @export
+is_tws_connected <- function(tws_con){
+  if (!is_tws_connection(tws_con)) {
+    stop("not a 'tws' connection", call. = FALSE)
+  }
+  
+  if (is_tws_connection_open(tws_con$con)){
+    glue("connection to tws is open and was opened at: {tws_con$connected_at}.")
+  } else {
+    glue("connection to tws is closed.")
+  }
 }
--- R/utils.R
+++ R/utils.R
@@ -8,9 +8,17 @@
 #' @examples
 #' print(tws_con)
 print.tws_con <- function(x, ...) {
-  if( isOpen(x[["con"]])) {
+  # TODO: rewrite to use glue and is_tws_connection_open
+  if (isOpen(x[["con"]])) {
     cat('<tws_connection,',x$clientId,' @ ',
         as.character(x$connected.at),'>\n', sep="")
   } else
     cat('<tws_connection, CLOSED>\n')
+}
+
+#' @keywords internal
+is_tws_connection_open <- function(con) {
+  if(inherits(try(isOpen(con), silent=TRUE), 'try-error')) {
+    FALSE
+  } else TRUE 
 }
