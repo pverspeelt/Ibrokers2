ea7b14ce7c5e6bedd7591e9bb4c917aac0b1ae79
pverspeelt <phiver4@gmail.com>
2019-03-21 18:00:57 +0100

    Add boolean transformation in make_field
    transform logical / boolean into integer values.

--- R/utils.R
+++ R/utils.R
@@ -24,6 +24,9 @@ is_tws_connection_open <- function(tws_con) {
 
 #' @keywords internal
 make_field <- function(x) {
+  if (is.logical(x)) {
+    return(as.integer(x))
+  } 
   if (is.null(x) || length(x) != 1L || is.na(x)) '' else as.character(x)
 }
 
